05f2cdcb644d87111202a743f8994485c4832ddd
Karim Rashad <karimrashad.official@gmail.com>
2024-09-17 06:49:59 +0300

    create splach screen

---
+++ turath/lib/features/splash/sconed_splach_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:turath/features/login/login_screen.dart';
+
+class SconedSplachScreen extends StatefulWidget {
+  const SconedSplachScreen({super.key});
+
+  @override
+  State<SconedSplachScreen> createState() => _SconedSplachScreenState();
+}
+
+class _SconedSplachScreenState extends State<SconedSplachScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const LoginScreen(),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xff1c1c1c),
+        body: Center(
+            child: Text(
+          "تُراث",
+          style: TextStyle(
+              fontSize: 100,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Fustat',
+              color: Color(0xffEDE4DD)),
+        )),
+      ),
+    );
+  }
+}
--- turath/lib/features/splash/splash_screen.dart
+++ turath/lib/features/splash/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:turath/core/utils/app_assets.dart';
+import 'package:turath/features/splash/sconed_splach_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +13,30 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SconedSplachScreen(),
+              ),
+            ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: const Color(0xff1c1c1c),
+          body: Center(
+            child: Image.asset(Assets.imagesLogo,
+                color: const Color(0xffEDE4DD),
+                width: 350,
+                height: double.infinity),
+          ),
+        ));
   }
 }
