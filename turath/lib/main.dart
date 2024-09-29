5e73ee7ea0bbda2d7fcc05019d2f84f504fd7e81
Karim Rashad <karimrashad.official@gmail.com>
2024-09-29 16:31:51 +0300

    shared preference for onboarding

--- turath/lib/main.dart
+++ turath/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:turath/core/database/cache/cache_helper.dart';
 import 'package:turath/core/routes/app_router.dart';
 import 'package:turath/core/utils/app_colors.dart';
 
-void main() => runApp(const TurathApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
+  runApp(const TurathApp());
+}
 
 class TurathApp extends StatelessWidget {
   const TurathApp({super.key});
