dc8955d274cb389be3fcd7ef650c45baea849a2e
Karim Rashad <karimrashad.official@gmail.com>
2024-10-10 21:51:07 +0300

    update

--- turath/lib/core/utils/app_assets.dart
+++ turath/lib/core/utils/app_assets.dart
@@ -6,17 +6,109 @@ class Assets {
   /// assets/images/3d-rendered-photo-ancient-books-adorn-library-carefully-arranged-with-classics_1158030-23026.jpg
   static const String images3dRenderedPhotoAncientBooksAdornLibraryCarefullyArrangedWithClassics115803023026 = "assets/images/3d-rendered-photo-ancient-books-adorn-library-carefully-arranged-with-classics_1158030-23026.jpg";
 
+  /// Assets for images6OctoberWar
+  /// assets/images/6OctoberWar.jpeg
+  static const String images6OctoberWar = "assets/images/6OctoberWar.jpeg";
+
+  /// Assets for imagesAdolfHitler
+  /// assets/images/AdolfHitler.jpg
+  static const String imagesAdolfHitler = "assets/images/AdolfHitler.jpg";
+
+  /// Assets for imagesBattleofAinJalut
+  /// assets/images/BattleofAinJalut.avif
+  static const String imagesBattleofAinJalut = "assets/images/BattleofAinJalut.avif";
+
+  /// Assets for imagesBattleofHattin
+  /// assets/images/BattleofHattin.jpg
+  static const String imagesBattleofHattin = "assets/images/BattleofHattin.jpg";
+
+  /// Assets for imagesBattleofYarmouk
+  /// assets/images/BattleofYarmouk.avif
+  static const String imagesBattleofYarmouk = "assets/images/BattleofYarmouk.avif";
+
+  /// Assets for imagesChina2
+  /// assets/images/china2.jpg
+  static const String imagesChina2 = "assets/images/china2.jpg";
+
+  /// Assets for imagesChina3
+  /// assets/images/china3.avif
+  static const String imagesChina3 = "assets/images/china3.avif";
+
+  /// Assets for imagesChineseCivilWar
+  /// assets/images/ChineseCivilWar.jpg
+  static const String imagesChineseCivilWar = "assets/images/ChineseCivilWar.jpg";
+
+  /// Assets for imagesChinesescreen
+  /// assets/images/chinesescreen.jpg
+  static const String imagesChinesescreen = "assets/images/chinesescreen.jpg";
+
+  /// Assets for imagesGenghisKhan
+  /// assets/images/GenghisKhan.jpg
+  static const String imagesGenghisKhan = "assets/images/GenghisKhan.jpg";
+
   /// Assets for imagesGoogle
   /// assets/images/google.svg
   static const String imagesGoogle = "assets/images/google.svg";
+
+  /// Assets for imagesGrecoPersianWars
+  /// assets/images/GrecoPersianWars.jpg
+  static const String imagesGrecoPersianWars = "assets/images/GrecoPersianWars.jpg";
+
+  /// Assets for imagesGreek2
+  /// assets/images/greek2.jpg
+  static const String imagesGreek2 = "assets/images/greek2.jpg";
+
+  /// Assets for imagesGreek3
+  /// assets/images/greek3.jpg
+  static const String imagesGreek3 = "assets/images/greek3.jpg";
+
+  /// Assets for imagesGreekscreen
+  /// assets/images/greekscreen.avif
+  static const String imagesGreekscreen = "assets/images/greekscreen.avif";
+
+  /// Assets for imagesHundredYearWar
+  /// assets/images/HundredYearWar.jpeg
+  static const String imagesHundredYearWar = "assets/images/HundredYearWar.jpeg";
 
   /// Assets for imagesIcon
   /// assets/images/icon.png
   static const String imagesIcon = "assets/images/icon.png";
 
+  /// Assets for imagesIslamic2
+  /// assets/images/islamic2.jpg
+  static const String imagesIslamic2 = "assets/images/islamic2.jpg";
+
+  /// Assets for imagesIslamic3
+  /// assets/images/islamic3.jpg
+  static const String imagesIslamic3 = "assets/images/islamic3.jpg";
+
+  /// Assets for imagesIslamic4
+  /// assets/images/islamic4.jpg
+  static const String imagesIslamic4 = "assets/images/islamic4.jpg";
+
+  /// Assets for imagesJulius
+  /// assets/images/Julius.jpg
+  static const String imagesJulius = "assets/images/Julius.jpg";
+
+  /// Assets for imagesKhaledibnElwaled
+  /// assets/images/khaledibnElwaled.jpeg
+  static const String imagesKhaledibnElwaled = "assets/images/khaledibnElwaled.jpeg";
+
   /// Assets for imagesLogo
   /// assets/images/logo.png
   static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesMohamedAli
+  /// assets/images/mohamedAli.webp
+  static const String imagesMohamedAli = "assets/images/mohamedAli.webp";
+
+  /// Assets for imagesMosque8328077640
+  /// assets/images/mosque-8328077_640.jpg
+  static const String imagesMosque8328077640 = "assets/images/mosque-8328077_640.jpg";
+
+  /// Assets for imagesNapoleon
+  /// assets/images/Napoleon.jpg
+  static const String imagesNapoleon = "assets/images/Napoleon.jpg";
 
   /// Assets for imagesOnboarding1image
   /// assets/images/onboarding1image.jpg
@@ -30,9 +122,49 @@ class Assets {
   /// assets/images/onboarding3image.jpg
   static const String imagesOnboarding3image = "assets/images/onboarding3image.jpg";
 
+  /// Assets for imagesPexelsPhoto16013193
+  /// assets/images/pexels-photo-16013193.webp
+  static const String imagesPexelsPhoto16013193 = "assets/images/pexels-photo-16013193.webp";
+
+  /// Assets for imagesPharaonic2
+  /// assets/images/Pharaonic2.jpg
+  static const String imagesPharaonic2 = "assets/images/Pharaonic2.jpg";
+
+  /// Assets for imagesPharaonic3
+  /// assets/images/Pharaonic3.avif
+  static const String imagesPharaonic3 = "assets/images/Pharaonic3.avif";
+
+  /// Assets for imagesPharaonic4
+  /// assets/images/Pharaonic4.jpg
+  static const String imagesPharaonic4 = "assets/images/Pharaonic4.jpg";
+
+  /// Assets for imagesPharaonicLiberation
+  /// assets/images/PharaonicLiberation.jpg
+  static const String imagesPharaonicLiberation = "assets/images/PharaonicLiberation.jpg";
+
+  /// Assets for imagesPharaonicscreen
+  /// assets/images/pharaonicscreen.avif
+  static const String imagesPharaonicscreen = "assets/images/pharaonicscreen.avif";
+
   /// Assets for imagesPlaceholder
   /// assets/images/placeholder.png
   static const String imagesPlaceholder = "assets/images/placeholder.png";
+
+  /// Assets for imagesQinShiHuang
+  /// assets/images/QinShiHuang.jpg
+  static const String imagesQinShiHuang = "assets/images/QinShiHuang.jpg";
+
+  /// Assets for imagesQutz
+  /// assets/images/Qutz.jpeg
+  static const String imagesQutz = "assets/images/Qutz.jpeg";
+
+  /// Assets for imagesRamses
+  /// assets/images/Ramses.jpg
+  static const String imagesRamses = "assets/images/Ramses.jpg";
+
+  /// Assets for imagesSalaheldin
+  /// assets/images/salaheldin.jpg
+  static const String imagesSalaheldin = "assets/images/salaheldin.jpg";
 
   /// Assets for imagesSp1
   /// assets/images/sp1.jpg
@@ -49,6 +181,10 @@ class Assets {
   /// Assets for imagesSp4
   /// assets/images/sp4.jpg
   static const String imagesSp4 = "assets/images/sp4.jpg";
+
+  /// Assets for imagesSplashicon
+  /// assets/images/splashicon.png
+  static const String imagesSplashicon = "assets/images/splashicon.png";
 
   /// Assets for imagesSwords
   /// assets/images/swords.svg
@@ -69,5 +205,13 @@ class Assets {
   /// Assets for imagesWars
   /// assets/images/wars.jpg
   static const String imagesWars = "assets/images/wars.jpg";
+
+  /// Assets for imagesWorldWarI
+  /// assets/images/WorldWarI.jpg
+  static const String imagesWorldWarI = "assets/images/WorldWarI.jpg";
+
+  /// Assets for imagesWorldwarII
+  /// assets/images/worldwarII.jpg
+  static const String imagesWorldwarII = "assets/images/worldwarII.jpg";
 }
 
